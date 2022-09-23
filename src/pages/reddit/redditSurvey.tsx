7805d7e13a5e2530b1cbb8dfe66c3734aa1d5743
Steve Li <lithafnium2000@gmail.com>
2022-09-22 21:06:10 -0400

    slider

--- src/pages/reddit/redditSurvey.tsx
+++ src/pages/reddit/redditSurvey.tsx
@@ -16,6 +16,9 @@ import {
 } from "./styles";
 import { Button } from "@app/shared/components/index";
 import { BsFillCaretLeftFill, BsFillCaretRightFill } from "react-icons/bs";
+import ReactSlider from "react-slider";
+import styled from "styled-components";
+import { colors } from "@app/styles/styles";
 
 interface Response {
   classification: string;
@@ -38,7 +41,61 @@ const NOVELTY = [
   "Almost no one can come up with my counterargument.",
 ];
 
+const StyledSlider = styled(ReactSlider)`
+  width: 100%;
+  height: 30px;
+`;
+
+const StyledThumb = styled.div`
+  font-family: "Source Sans Pro", sans-serif;
+
+  height: 25px;
+  line-height: 25px;
+  width: 25px;
+  text-align: center;
+  background-color: #ddd;
+  color: #000;
+  border-radius: 50%;
+  border: 1px solid ${colors.PRIMARY};
+  cursor: grab;
+`;
+//@ts-ignore
+const Thumb = (props, state) => (
+  <StyledThumb {...props}>{state.valueNow}</StyledThumb>
+);
+
+const StyledMark = styled.div`
+  cursor: pointer;
+  top: 6px;
+  width: 5px;
+  height: 8px;
+  background-color: ${colors.PRIMARY};
+`;
+//@ts-ignore
+const Mark = (props) => <StyledMark {...props} />;
+
+const StyledTrack = styled.div`
+  top: 9px;
+  bottom: 0;
+  background: ${colors.PRIMARY};
+  border-radius: 999px;
+  height: 5px;
+`;
+
+//@ts-ignore
+const Track = (props, state) => <StyledTrack {...props} index={state.index} />;
+
+const StyledContainer = styled.div`
+  // resize: horizontal;
+  overflow: auto;
+  width: 100%;
+  max-width: 100%;
+  padding-right: 8px;
+`;
+
 const RedditSurvey = () => {
+  const [currentValue, setCurrentValue] = useState(0);
+
   const [responses, setResponses] = useState<Response[]>([]);
   const [showA, setShowA] = useState(false);
   const [current, setCurrent] = useState(null);
@@ -87,7 +144,7 @@ const RedditSurvey = () => {
             Based on your reasoning, please classify the data point into one of
             the following categories.
           </h3>
-          <Content style={{ fontSize: "1em" }}>
+          <div style={{ fontSize: "1em", margin: "1em 0 1em 0" }}>
             {CATEGORIES.map((e, i) => {
               return (
                 <SelectionRow key={i}>
@@ -105,29 +162,39 @@ const RedditSurvey = () => {
                 </SelectionRow>
               );
             })}
-          </Content>
+          </div>
           <Divider />
           {showA && (
             <div>
               <h3>Enter in the "novelty" level of your counterargument</h3>
-              <RowContainer>
-                {NOVELTY.map((e, i) => {
-                  return (
-                    <div
-                      key={i}
-                      style={{
-                        display: "flex",
-                        justifyContent: "center",
-                        flexDirection: "column",
-                      }}
-                    >
-                      <input type="radio" id="html" name="fav_language" />
-                      <p style={{ textAlign: "center" }}>{e}</p>
-                    </div>
-                  );
-                })}
-              </RowContainer>
-              <Divider />
+              <StyledContainer>
+                <StyledSlider
+                  // defaultValue={[50, 75]}
+                  min={1}
+                  max={5}
+                  marks
+                  renderTrack={Track}
+                  renderThumb={Thumb}
+                  renderMark={Mark}
+                  onChange={(value, index) =>
+                    console.log(`onChange: ${JSON.stringify({ value, index })}`)
+                  }
+                />
+              </StyledContainer>
+              <div
+                style={{
+                  display: "flex",
+                  width: "100%",
+                  justifyContent: "space-between",
+                }}
+              >
+                <p style={{ width: "15%" }}>
+                  Almost everyone can come up with my counterargument.
+                </p>
+                <p style={{ width: "15%" }}>
+                  Almost no one can come up with my counterargument.
+                </p>
+              </div>
             </div>
           )}
           <div>
